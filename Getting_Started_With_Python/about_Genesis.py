98726a1e9149060e162235d633b4b676bcb9e047
ggreen06 <111703471+ggreen06@users.noreply.github.com>
2022-10-15 17:52:19 -0700

    Submitting first assignment

---
+++ Getting_Started_With_Python/about_Genesis.py
@@ -0,0 +1,16 @@
+full_name = "Genesis Green"
+nickname = "GG"
+age = 16
+has_used_python = True
+
+hobbies = ["reading", "working out", "playing video games", "taking pictures", "practicing selfcare", "hanging out with my friends"]
+
+fav_things = {
+  "movie": "10 Things I Hate About You",
+  "food": "pho",
+  "hobby": hobbies[0],
+  "show": "Gilmore Girls",
+  "number": 8
+}
+
+all_vars = dict(vars())
